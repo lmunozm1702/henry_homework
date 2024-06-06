24d67641e0d20c62f0b2d61b2eabfa66daa1f24c
Luis Muñoz <l.munoz.m@outlook.com>
2024-06-06 19:20:37 -0400

    M2-C1 baseline

--- M1/Clase 04/hw04.py
+++ M1/Clase 04/hw04.py
@@ -1,15 +1,3 @@
-import pandas as pd
-import csv 
-df = pd.read_csv('./M1/Clase 04/hospitales2.csv')
-
-df['WKT'] = df['WKT'].str.replace('POINT ', '')
-df['WKT'] = df['WKT'].str.replace('(', '')
-df['WKT'] = df['WKT'].str.replace(')', '')
-
-latitude = df['WKT'].str.split(' ', expand=True)[0]
-longitude = df['WKT'].str.split(' ', expand=True)[1]
-
-datos = dict(latitude=latitude, longitude=longitude, name=df['DOM_NORMA'], label=df['NOM_MAP'])
-print(datos)
-
-pd.DataFrame(datos).to_csv('hospitales2_salida.csv', index=False)
+import numpy as np
+arr = np.zeros(3)
+print(arr)
