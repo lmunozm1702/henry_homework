1682218f84cdd233c3402067377e7b4d75f7fd5e
Luis Muñoz <l.munoz.m@outlook.com>
2024-06-17 06:10:25 -0400

    Create hw3.py

---
+++ M2/Clase 03/hw3.py
@@ -0,0 +1,14 @@
+# Suponga dos eventos, A y B, y que P(A) = 0.50, P(B) = 0.60 y P(A ∩ B) = 0.40.<br>
+# a. calcule P(A | B).
+# P(A | B) = P(A ∩ B) / P(B)
+print("P(A | B) = P(A ∩ B) / P(B) = 0.40 / 0.60 = ", 0.40 / 0.60)
+
+# Halle P(B | A).
+# P(B | A) = P(A ∩ B) / P(A)
+print("P(B | A) = P(A ∩ B) / P(A) = 0.40 / 0.50 = ", 0.40 / 0.50)
+
+#P(AyB) = P(A) *  P(B)
+print("P(AyB) = P(A) *  P(B) = 0.50 * 0.60 = ", 0.50 * 0.60)
+
+# c. ¿A y B son independientes? ¿Por qué sí o por qué no?
+
